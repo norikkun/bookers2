3bd1ac2ba7db5b697952729d37ad03e23e8df8e3
norikkun <161901725+norikkun@users.noreply.github.com>
2024-04-11 15:26:13 +0000

    [update] bookの左側作成途中まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,24 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
+    @user = current_user
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to user_path(@book)
   end
 
-  def new
-  end
 
   def show
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
